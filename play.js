e7c681c3e3119e6fdb9b63d5ee4769af56ea4cdb
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-12 10:13:26 -0500

    added function connect to play.js

--- play.js
+++ play.js
@@ -1,4 +1,28 @@
 #!/usr/bin/env node
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Received data from server: ", data);
+  });
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
 
 process.stdout.write('\x07');
 
