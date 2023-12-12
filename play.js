c1ca138156a1ac887b94439c87d93e62ea031cb0
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-12 12:16:05 -0500

    added handleUserInput function in play.js

--- play.js
+++ play.js
@@ -5,6 +5,27 @@ console.log("Connecting ...");
 connect();
 
 
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
+
+
 process.stdout.write('\x07');
 
 const { Game } = require('./src/Game')
@@ -13,4 +34,4 @@ const { RemoteInterface } = require('./src/RemoteInterface')
 const game = new Game(new UserInterface(), new RemoteInterface())
 
 // Begin game
-game.start()
+  game.start()
