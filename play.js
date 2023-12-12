bdd7249afd53e2a1398d4c408ade73f5c23bfed8
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-12 12:24:10 -0500

    exported setupInput function to play.js from input.js

--- play.js
+++ play.js
@@ -1,30 +1,11 @@
 #!/usr/bin/env node
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 connect();
 
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  const handleUserInput = function (data) {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  }
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
 setupInput();
-
 
 process.stdout.write('\x07');
 
