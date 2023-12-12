b1935292e6e9af05430c0d7d190372af8d0a465a
mariefranceperrier <mariefrance.perrier@gmail.com>
2023-12-12 10:31:41 -0500

    moved function connect to client.js +require {connect} in play.js

---
+++ client.js
@@ -0,0 +1,22 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.on("data", (data) => {
+    console.log("Received data from server: ", data);
+  });
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,24 +1,5 @@
 #!/usr/bin/env node
-const net = require("net");
-
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("Successfully connected to game server");
-  });
-
-  conn.on("data", (data) => {
-    console.log("Received data from server: ", data);
-  });
-  return conn;
-};
+const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
